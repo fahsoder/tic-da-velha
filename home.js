af84ad526923cf50f6f3f503e1f8e559d0df09e3
Fabiano Soder <fabianosoder@hotmail.com>
2019-06-10 21:34:13 -0300

    add react app

--- home.js
+++ home.js
@@ -1,9 +1,20 @@
 function selectPlayer(numberOfPlayers) {
     if (numberOfPlayers === 1) {
-        document.getElementById('singlePlayerGame').setAttribute('hidden', false);
-        document.getElementById('multiPlayerGame').setAttribute('hidden', true);
+        console.log('xalxalxa', 1);
+        document.getElementById('singlePlayerGame').removeAttribute('hidden');
+        document.getElementById('multiPlayerGame').setAttribute('hidden', "true");
+        document.getElementById('select-mode').setAttribute('hidden',  "true");
     } else {
-        document.getElementById('singlePlayerGame').setAttribute('hidden', true);
-        document.getElementById('multiPlayerGame').setAttribute('hidden', false);
+        console.log('xalxalxa', 2);
+        document.getElementById('singlePlayerGame').setAttribute('hidden');
+        document.getElementById('multiPlayerGame').removeAttribute('hidden');
+        document.getElementById('select-mode').setAttribute('hidden');
     }
+}
+
+function voltarSelecaoModo() {
+    console.log('xalxalxa', 123);
+    document.getElementById('singlePlayerGame').setAttribute('hidden');
+    document.getElementById('multiPlayerGame').setAttribute('hidden');
+    document.getElementById('select-mode').removeAttribute('hidden');
 }
